96444dc87fd486dbbe309c7ab5884e30805fe329
Your Name <myemail@gmail.com>
2023-02-21 07:28:34 -0800

    104-Fiba

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,9 +13,9 @@ int main(void)
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (count = 0; count < 92, count++)
+	for (count = 0; count < 92; count++)
 	{
-		sum = fib1 + fib 2;
+		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 		fib1 = fib2;
 		fib2 = sum;
@@ -34,9 +34,9 @@ int main(void)
 			half2 %= 10000000000;
 
 		}
-		printf("%lu%lu", half1 , half2);
-		if ( count != 98)
-			printf(",");
+		printf("%lu%lu", half1 ,half2);
+		if (count != 98)
+	        printf(", ");
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
