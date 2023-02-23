fcb280e31fe91cb3b1b9a21fccba7ee2ba613ead
Your Name <myemail@gmail.com>
2023-02-23 08:12:03 -0800

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more numbers prints 10 time of numbers since 0 up to 14
+ * Return: 10 time of numbers since 0 up to 14
+ */
+
+void more_numbers(void)
+
+{
+	int x, y; 
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 14; y++)
+		{
+        if (y > 9)
+        {
+                _putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+        }
+		_putchar('\n');
+	}
+}
