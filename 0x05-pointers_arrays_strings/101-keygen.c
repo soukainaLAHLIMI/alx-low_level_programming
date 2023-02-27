f6bbe4bbd0dd1c36f13ffa504fa699c5ab96c77e
Your Name <myemail@gmail.com>
2023-02-27 13:52:29 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -31,7 +31,7 @@ int main(void)
 			diff_half1++;
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diffhalf1))
+			if (password[index] >= (33 + diff_half1))
 			{
 			password [index] -= diff_half1;
 		break;
@@ -39,7 +39,7 @@ int main(void)
 		}
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diffhalf2))
+			if (password[index] >= (33 + diff_half2))
                         {
 				password [index] -= diff_half2;
 				break;
