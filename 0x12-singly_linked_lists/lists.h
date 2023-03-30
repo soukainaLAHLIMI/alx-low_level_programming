674266655de8e7957c66c1447f7434ea2555191e
Your Name <s.lahlimi.95@gmail.com>
2023-03-30 07:16:31 -0700

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,34 +29,3 @@ void free_list(list_t *head);
 
 #endif
 
-Note: please sure you check it with betty  style of code
-
-and compile it with it gcc lists.h
-
-
-vi 0-print_list.c
-#include <stdio.h>
-#include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
-
-size_t print_list(const list_t *h)
-{
-	size_t nelem;
-
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
-	}
-	return (nelem);
-}
-
